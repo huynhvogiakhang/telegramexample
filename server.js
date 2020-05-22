ad4c2e93c8e1626e2c1511375366e14f2a5691e8
Khang Huynh <khanghvg@fimplus.vn>
2020-05-22 09:34:39 +0700

    add webhook route

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
+const request = require('request')
 
-console.log("running")
+app.get('/webhook', (req, res) => {
+  request('https://gateway.chotot.com/v1/public/ad-listing?cg=1000&limit=20&st=s,k', (error, res, body) => {
+    let x = JSON.parse(body)
 
+  })
+  res.send("ok")
+})
 
 app.listen(3000)
